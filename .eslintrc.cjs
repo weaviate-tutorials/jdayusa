1c2560f9a866ea073ca6f97cc1cca46ae56cdaef
Dan Dascalescu <dan@weaviate.io>
2023-05-18 22:51:21 +0100

    Add rules to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,14 @@ module.exports = {
     semi: ['error', 'always'],
     quotes: ['warn', 'single', { avoidEscape: true }],
     'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'space-in-parens': ['error', 'never'],
+    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never' }],
     'no-trailing-spaces': 'warn',
+    'spaced-comment': ['warn', 'always'],  // start comment with a space
+    'object-curly-spacing': ['error', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'prefer-const': 'warn',
+    'no-use-before-define': 'error',
+    'no-shadow': 'warn',
   }
 };
